3b7899a561ace3923caca6104c1a176157811373
Cameron Thomson <102567280+19009ct@users.noreply.github.com>
2023-03-30 22:52:21 +0000

    Added a good parralax with multiple images

--- script.js
+++
@@ -1,6 +0,0 @@
-function parallax() {
-  var parallax = document.getElementById("parallax");
-  parallax.style.top = -(window.pageYOffset / 4)+'px';
-}
-
-window.addEventListener("scroll", parallax, false);
